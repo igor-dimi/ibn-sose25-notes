01c487f224b7d923cdfcc55097af8e6446c3c8d9
Igor Dimitrov <igor-dimi@protonmail.com>
2025-04-28 15:57:11 +0200

    add threads example, chnage makefile w02

---
+++ code/w03/threads_ex.c
@@ -0,0 +1,38 @@
+#include <pthread.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <assert.h>
+#include <stdlib.h>
+#define NUM_THREADS 1000
+
+void* TaskCode (void* argument)
+{
+    int tid; tid = *((int* ) argument);
+    printf("%d: ", tid);
+    for (int i = 0; i < 51; i++) printf("%d ", i);
+    printf("\n");
+    return NULL;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    pthread_t threads[NUM_THREADS];
+    int thread_args[NUM_THREADS]; 
+    int rc;
+
+    for (int i = 0; i < NUM_THREADS; i++) { /* create all threads */
+        thread_args[i] = i;
+        // printf("In main: creating thread %d\n", i);
+        rc = pthread_create(&threads[i], NULL, TaskCode, (void* ) &thread_args[i]);
+        assert(0 == rc);
+    }
+
+    // wait for all threads to complete
+    for (int i = 0; i < NUM_THREADS; i++) {
+        rc = pthread_join(threads[i], NULL);
+        assert(0 == rc);
+    }
+
+    exit(0);
+}
